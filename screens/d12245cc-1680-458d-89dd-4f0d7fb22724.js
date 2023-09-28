743fcba10e3ef1da5f7bc182007ade6902ee44fb
shrouqAlharbi <93242042+shrouqAlharbi@users.noreply.github.com>
2023-09-28 13:45:50 +0300

    Add files via upload

---
+++ screens/d12245cc-1680-458d-89dd-4f0d7fb22724.js
@@ -0,0 +1,137 @@
+var content='<div class="ui-page" deviceName="web" deviceType="desktop" deviceWidth="1000" deviceHeight="768">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devWeb canvas firer commentable non-processed" alignment="left" name="Template 1" width="1000" height="768">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1695895592592.css" />\
+      <div class="freeLayout">\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+    <div id="s-d12245cc-1680-458d-89dd-4f0d7fb22724" class="screen growth-vertical devWeb canvas PORTRAIT firer ie-background commentable non-processed" alignment="left" name="Screen 1" width="1000" height="768">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1695895592592.css" />\
+      <div class="freeLayout">\
+      <div id="s-Image_1" class="image firer ie-background commentable non-processed" customid="التقاط"   datasizewidth="998.5px" datasizeheight="53.5px" dataX="0.7" dataY="-0.0"   alt="image">\
+        <div class="borderLayer">\
+        	<div class="imageViewport">\
+        		<img src="./images/e085d7e5-da33-4445-95aa-acb324751de7.PNG" />\
+        	</div>\
+        </div>\
+      </div>\
+\
+      <div id="s-Button_2" class="button multiline manualfit firer commentable non-processed" customid="التالي"   datasizewidth="68.0px" datasizeheight="35.0px" dataX="32.0" dataY="107.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Button_2_0">التالي</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Button_3" class="button multiline manualfit firer commentable non-processed" customid="للخلف"   datasizewidth="68.0px" datasizeheight="35.0px" dataX="118.0" dataY="107.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Button_3_0">للخلف</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Button_4" class="button multiline manualfit firer commentable non-processed" customid="الغاء"   datasizewidth="68.0px" datasizeheight="35.0px" dataX="203.0" dataY="107.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Button_4_0">الغاء</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Paragraph_1" class="richtext manualfit firer ie-background commentable non-processed" customid="طلب ايفاد : البيانات الاي"   datasizewidth="215.0px" datasizeheight="38.5px" dataX="759.0" dataY="86.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Paragraph_1_0">طلب ايفاد : البيانات الايضافية </span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Paragraph_2" class="richtext manualfit firer ie-background commentable non-processed" customid="اسم الموظف : شروق"   datasizewidth="215.0px" datasizeheight="38.5px" dataX="467.0" dataY="124.5" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Paragraph_2_0">اسم الموظف : شروق</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Paragraph_3" class="richtext manualfit firer ie-background commentable non-processed" customid="عنوان البريد الالكتروني ل"   datasizewidth="369.0px" datasizeheight="38.5px" dataX="313.0" dataY="163.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Paragraph_3_0">عنوان البريد الالكتروني للمنشأة : sfeharbi@moh.gov.sa</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Button_1" class="button multiline manualfit firer commentable non-processed" customid="اضافة"   datasizewidth="68.0px" datasizeheight="35.0px" dataX="855.0" dataY="323.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Button_1_0">اضافة</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
