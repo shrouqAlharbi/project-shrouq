b7a613302afc466468d8aad914119efaa0efab40
shrouqAlharbi <93242042+shrouqAlharbi@users.noreply.github.com>
2023-09-28 13:49:55 +0300

    Delete includes directory

--- includes/include-1695895592592.js
+++
@@ -1,3 +0,0 @@
-document.write("<script type='text/javascript' charset='utf-8' src='./resources/jim/javascript/jim-min.js'></script>");
-document.write("<script type='text/javascript' charset='utf-8' src='./resources/scenarios/function-jim-links-1695895592592.js'></script>");
-document.write("<script type='text/javascript' charset='utf-8' src='./resources/prototype-1695895592592.js'></script>");
