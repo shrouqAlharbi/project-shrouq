8c918c87d63f664ec3666aa061f34407df857484
shrouqAlharbi <93242042+shrouqAlharbi@users.noreply.github.com>
2023-09-28 13:44:19 +0300

    Add files via upload

---
+++ review/screens/3ee17948-1506-48bd-9654-8ba5a4faf86e.js
@@ -0,0 +1,32 @@
+var content='<div class="ui-page" deviceName="web" deviceType="desktop" deviceWidth="1000" deviceHeight="768">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devWeb canvas firer commentable non-processed" alignment="left" name="Template 1" width="1000" height="768">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1695895592592.css" />\
+      <div class="freeLayout">\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+    <div id="s-3ee17948-1506-48bd-9654-8ba5a4faf86e" class="screen growth-vertical devWeb canvas PORTRAIT firer ie-background commentable non-processed" alignment="left" name="تطبيق" width="1000" height="768">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/3ee17948-1506-48bd-9654-8ba5a4faf86e-1695895592592.css" />\
+      <div class="freeLayout">\
+      <div id="s-Image_1" class="image firer ie-background commentable non-processed" customid="التقاط"   datasizewidth="998.5px" datasizeheight="53.5px" dataX="0.7" dataY="-0.0"   alt="image">\
+        <div class="borderLayer">\
+        	<div class="imageViewport">\
+        		<img src="./images/e085d7e5-da33-4445-95aa-acb324751de7.PNG" />\
+        	</div>\
+        </div>\
+      </div>\
+\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
